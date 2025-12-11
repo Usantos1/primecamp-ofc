565cb7e2700ca32032de71d8a7da3eac052b13f6
Usantos1 <uander9sts@gmail.com>
2025-12-11 07:17:01 -0300

    fix: copiar arquivos assistencia do worktree

--- src/pages/assistencia/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as OrdensServico } from './OrdensServico';
-export { default as OrdemServicoForm } from './OrdemServicoForm';
-export { default as Clientes } from './Clientes';
-export { default as Produtos } from './Produtos';
