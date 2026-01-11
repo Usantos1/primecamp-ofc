92e16fadd93573ad813d7618b7202473c2e781ed
Usantos1 <uander9sts@gmail.com>
2026-01-11 13:50:55 -0300

    fix: adicionar Toaster de volta para exibir toasts do useToast hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { Toaster } from "@/components/ui/toaster"; // Temporariamente desabilitado - usando apenas Sonner
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -90,7 +90,7 @@ const App = () => (
             <AuthProvider>
               <NotificationManager>
               <TooltipProvider>
-                {/* <Toaster /> Temporariamente desabilitado - usando apenas Sonner */}
+                <Toaster />
                 <Sonner />
                 <BrowserRouter>
             <Routes>
